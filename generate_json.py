21e118d12ddcf3bed1e5b31cd6b10c38059ea547
Tom Lynch <twhlynch@gmail.com>
2023-09-19 11:16:31 +1000

    update

--- generate_json.py
+++ generate_json.py
@@ -3,7 +3,8 @@ import json
 from git import Repo
 
 repo_path = "../grab-tools.live"
-file_name = "public/stats_data/all_verified.json"
+file_name = "stats_data/all_verified.json"
+alt_file_name = "public/stats_data/all_verified.json"
 output_dir = "downloaded_versions"
 
 if not os.path.exists(output_dir):
@@ -11,12 +12,12 @@ if not os.path.exists(output_dir):
 
 repo = Repo(repo_path)
 
-for commit in repo.iter_commits(paths=file_name):
+def download_commit(commit, file_path):
     commit_hash = commit.hexsha
     commit_date = commit.committed_date
     commit_message = commit.message.strip()
 
-    file_content = repo.git.show(f"{commit_hash}:{file_name}")
+    file_content = repo.git.show(f"{commit_hash}:{file_path}")
     version_filename = f"{commit_date}_{commit_message}.txt"
     version_filepath = os.path.join(output_dir, version_filename)
 
@@ -24,7 +25,7 @@ for commit in repo.iter_commits(paths=file_name):
         f.write(file_content)
 
     json_data = {
-        "filename": file_name,
+        "filename": file_path,
         "date": commit_date,
         "message": commit_message,
         "hash": commit_hash
@@ -34,5 +35,14 @@ for commit in repo.iter_commits(paths=file_name):
 
     with open(json_filepath, "w") as f:
         json.dump(json_data, f, indent=4)
+
+for commit in repo.iter_commits(paths=[file_name, alt_file_name]):
+    try:
+        if file_name in commit.stats.files:
+            download_commit(commit, file_name)
+        elif alt_file_name in commit.stats.files:
+            download_commit(commit, alt_file_name)
+    except Exception as e:
+        print(f"Error: {e}")
 
 print("All versions downloaded with metadata.")
